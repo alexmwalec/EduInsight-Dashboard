308bbccde11d1962150266a380ea6c197cdffd61
Alexmwalec <alexmwalec03@gmail.com>
2025-05-09 09:40:13 +0200

    Addition of Components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TeacherSignUp from "./Components/teacherSignUp/teacherSignUp";
 import TeacherLogIn from "./Components/TeacherLogin/teacherLogIn";
-import Dashboard from "./Components/Teacher-Dashboard/Dashboard";
+import Dashboard from "./Components/Teacher-Dashboard/TeacherDashboard";
 
 function App() {
   return (
